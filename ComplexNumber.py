aea4295d6ee1c9c11bb807e439542e28797dbfe0
sammaelcz <jakubkrocil@gmail.com>
2025-02-15 06:37:15 +0100

    refactor: 1) simplify get_number_input 2) simplify main prints

--- ComplexNumber.py
+++ ComplexNumber.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
 
     def get_number_input():
         while True:
-            choice_prompt = input("Set number type: 'i' = int, 'f' = float, 'c' = complex: ")
+            choice_prompt = input("Set number type: 'i' = int, 'f' = float, 'c' = complex: ").lower()
             try:
                 if choice_prompt == "i":
                     return ComplexNumber(int(input("Enter integer (example: 32): ")))
@@ -83,43 +83,40 @@ if __name__ == '__main__':
                     return ComplexNumber(float(input("Enter float (example: 3.54): ")))
                 elif choice_prompt == "c":
                     return ComplexNumber(complex(input("Enter complex number (example: 3+4j): ")))
-                else:
-                    continue
             except ValueError:
                 print("You entered a wrong value. Try again.")
 
     # __init__
-
     print("Define first number:")
     num1 = get_number_input()
     print("Define second number:")
     num2 = get_number_input()
 
-    print("\n__eq__")
-    print(f"Is {num1} == {num2} ? {num1 == num2}")
+    # __eq__
+    print(f"\nEquality:\n{num1} == {num2} ? {num1 == num2}")
 
-    print("\n__lt__")
-    print(f"Is {num1} < {num2} ? {num1 < num2}")
+    # __lt__
+    print(f"\nLesser than:\n{num1} < {num2} ? {num1 < num2}")
 
-    print("\n__gt__")
-    print(f"Is {num1} > {num2} ? {num1 > num2}")
+    # __gt__
+    print(f"\nGreater than:\n{num1} > {num2} ? {num1 > num2}")
 
-    print("\nAddition")
-    print(f"{num1} + {num2} = {num1 + num2}")
+    # __add__
+    print(f"\nAddition:\n{num1} + {num2} = {num1 + num2}")
 
-    print("\nSubtraction")
-    print(f"{num1} - {num2} = {num1 - num2}")
+    # __sub__
+    print(f"\nSubtraction\n{num1} - {num2} = {num1 - num2}")
 
-    print("\nMultiplication")
-    print(f"{num1} * {num2} = {num1 * num2}")
+    # __mul__
+    print(f"\nMultiplication\n{num1} * {num2} = {num1 * num2}")
 
-    print("\nDivision")
-    print(f"{num1} / {num2} = {num1 / num2}")
+    # __truediv__
+    print(f"\nDivision\n{num1} / {num2} = {num1 / num2}")
 
-    # Conjugation
-    print(f"\nConjugation of {num1} = {num1.conjugate_value()}")
-    print(f"\nConjugation of {num2} = {num2.conjugate_value()}")
+    # conjugate_value
+    print(f"\nConjugation of {num1} = {num1.conjugate_value()}"
+          f"\nConjugation of {num2} = {num2.conjugate_value()}")
 
-    # Absolute Value
-    print(f"\nAbsolute value of {num1} = {num1.absolute_value()}")
-    print(f"\nAbsolute value of {num2} = {num2.absolute_value()}")
+    # absolute_value
+    print(f"\nAbsolute value of {num1} = {num1.absolute_value()}"
+          f"\nAbsolute value of {num2} = {num2.absolute_value()}")
